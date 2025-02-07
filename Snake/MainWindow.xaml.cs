2eed1055df1e4a2489f47939d919a28151504995
Damian <damian.wakulak69@gmail.com>
2025-02-07 20:26:21 +0100

    Snake and Food Visualization

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -16,9 +16,58 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Rectangle> snake;
+        int snakeSize = 32;
+        int score = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+            StartGame();
         }
+
+        private Rectangle CreateRectangle(bool isHead)
+        {
+            Rectangle rect  = new Rectangle
+            {
+                Width = snakeSize,
+                Height = snakeSize,
+                Fill = isHead ? Brushes.LimeGreen : Brushes.Green,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            return rect;
+        }
+
+        private void StartGame()
+        {
+            GameCanvas.Children.Clear();
+
+            snake = new List<Rectangle>();
+            Rectangle head = CreateRectangle(true);
+            GameCanvas.Children.Add(head);
+            score = 0;
+
+            Canvas.SetLeft(head, 100);
+            Canvas.SetTop(head, 100);
+
+            Rectangle food = CreateFood();
+            GameCanvas.Children.Add(food);
+
+        }
+
+        private Rectangle CreateFood()
+        {
+            Rectangle food = new Rectangle
+            {
+                Width = snakeSize,
+                Height = snakeSize,
+                Fill = Brushes.Red,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            return food;
+        }
+
     }
 }
